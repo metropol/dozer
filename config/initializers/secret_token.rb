a05af46a63a79b6232a8ac8b50c609dca534e85e
Ugo Di Profio <gmetropol@gmail.com>
2011-04-24 12:49:53 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dozer::Application.config.secret_token = 'b8daffeb1d772f6c8e92691a111abc6df60fce779507d4a1ab1d808a2856ef43eb272ff602394ca9eb31b4f02b6cec387edd446ce7d4d3c7e1059ffaf79a6325'
